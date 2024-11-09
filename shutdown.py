75759a176d09905bc7330dcd9b4610aef9d22861
6579321 <yasmeen.shaik99@gmail.com>
2024-11-09 12:38:46 -0500

    down

---
+++ shutdown.py
@@ -0,0 +1,7 @@
+def shutdown():
+    print("System is shutting down... Please wait.")
+    # Add more logic here if you want to simulate the process
+    print("Shutdown complete.")
+
+# Call the shutdown function
+shutdown()
