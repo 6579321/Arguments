1d199e7caee9f894d6dc7543ac07f9a456b3e02c
6579321 <yasmeen.shaik99@gmail.com>
2024-11-09 12:19:26 -0500

    birthday

---
+++ birthday.py
@@ -0,0 +1,13 @@
+# Storing birthday dates in different variables
+birthday_person1 = "January 15, 1990"
+birthday_person2 = "March 22, 1985"
+birthday_person3 = "June 5, 2000"
+birthday_person4 = "September 12, 1992"
+birthday_person5 = "November 30, 1988"
+
+# Printing the birthday dates
+print("Birthday of Person 1:", birthday_person1)
+print("Birthday of Person 2:", birthday_person2)
+print("Birthday of Person 3:", birthday_person3)
+print("Birthday of Person 4:", birthday_person4)
+print("Birthday of Person 5:", birthday_person5)
